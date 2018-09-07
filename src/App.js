d590e8f441623e1de45cbb83d88971487626c0ca
Vincent, Erin <evincent@ariit.com>
2018-09-07 11:30:27 -0600

    initial commmit

---
+++ src/App.js
@@ -0,0 +1,45 @@
+import React, {Component} from 'react';
+import logo from './logo.svg';
+import './App.css';
+import {loadModules, loadCss} from 'esri-loader';
+
+loadCss('https://js.arcgis.com/4.8/esri/css/main.css');
+
+class App extends Component {
+
+  componentDidMount() {
+    loadModules(['esri/Map', 'esri/views/MapView']).then(([Map, MapView]) => {
+
+      const map = new Map({
+        basemap: 'streets'
+      });
+
+      const mapView = new MapView({
+        container: 'mapDiv',
+        map,
+        zoom: 3
+      });
+
+      this.setState({
+        map
+      });
+
+    });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo"/>
+          <h1 className="App-title">Welcome to React</h1>
+        </header>
+        <div id="mapDiv" style={{height: 'calc(100vh - 190px)'}}>
+
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
